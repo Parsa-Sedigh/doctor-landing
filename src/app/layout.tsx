403cab9c75292367c1e9717fdbd008dfc67cc7bc
parsa <parsa.sedigh78@gmail.com>
2024-07-04 22:08:51 +0330

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import {Header} from "@/components/Header/Header";
 import {Footer} from "@/components/Footer/Footer";
 
 const yekanFont = localFont({
-    src: './Yekan.ttf',
+    src: './yekan.ttf',
     display: 'swap',
     variable: '--font-yekan'
 })
