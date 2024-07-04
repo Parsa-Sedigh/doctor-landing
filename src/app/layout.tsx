cc8414a0094daa4e9f39e0d898c7b379548dd101
parsa <parsa.sedigh78@gmail.com>
2024-07-04 22:05:14 +0330

    correct path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import {Header} from "@/components/Header/Header";
 import {Footer} from "@/components/Footer/Footer";
 
 const yekanFont = localFont({
-    src: './yekan.ttf',
+    src: './Yekan.ttf',
     display: 'swap',
     variable: '--font-yekan'
 })
